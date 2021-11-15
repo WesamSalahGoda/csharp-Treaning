b5d61aa5904d6b17b1b1f6e588858991d637cfb0
Mohammed Abdelhay <mamado2000@gmail.com>
2021-11-15 21:11:15 +0200

    add heelo for my self

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Wesam Salah!");
+            Console.WriteLine("Hello Mohammed Abdelhay!");
         }
     }
 }
