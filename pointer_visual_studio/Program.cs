d47190e06b6ac4a2d18dc246444919469f69690f
Wesam Salah Goda <oo_wesam_oo@hotmail.com>
2021-11-16 23:43:01 +0200

    Pointers in Vesual Studio And Allow unsafe code

---
+++ pointer_visual_studio/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace pointer_visual_studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = 'W';
+            unsafe
+            {
+                char* ptr = &a;
+                Console.WriteLine((long)ptr);
+                Console.WriteLine(*ptr);
+            }
+        }
+    }
+}
---
+++ var_local_variable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace var_local_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
