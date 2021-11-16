29a5857ce6741e58906b3d37917812817ebedc8b
Wesam Salah Goda <oo_wesam_oo@hotmail.com>
2021-11-17 00:01:04 +0200

    implicitly type local variable (var)

--- var_local_variable/Program.cs
+++ var_local_variable/Program.cs
@@ -6,7 +6,16 @@ namespace var_local_variable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char a1 = 'W';
+            var a2 = 'M';
+            Console.WriteLine(a2);
+            Console.WriteLine("The Type of a2 {0}", a2.GetType().ToString());
+            var agree = true;
+            Console.WriteLine("The Type of agree {0}", agree.GetType().ToString());
+            var courses = new[] { "Wesam", "Salah", "Anas" };
+            Console.WriteLine("The Type of courses {0}", courses.GetType().ToString());
+            var s1 = new { name = "Wesam Salah", age = 47 };
+            Console.WriteLine("my name is {0}", s1.name);
         }
     }
 }
